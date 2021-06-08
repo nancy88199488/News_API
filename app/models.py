2597795b3833b805049affc3e8979b89b88036f5
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 15:53:45 +0300

    format the files

--- app/models.py
+++ app/models.py
@@ -1,35 +1,35 @@
 class NewsArticle:
-    '''
-    News article class to define the newsArticle object 
-    '''
-def __init__(self,author,description,time,url,image,title):
-        self.id = id
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
+        '''
+        News article class to define the newsArticle object 
+        '''
+        def __init__(self,author,description,time,url,image,title):
+                self.id = id
+                self.author = author
+                self.description = description
+                self.time = time
+                self.url = url
+                self.image = image
+                self.title = title
 
 class NewsSource:
-     '''
-       News source class to define the newsSource object 
-       '''
-def __init__(self,id,name,description,url):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
+        '''
+        News source class to define the newsSource object 
+        '''
+        def __init__(self,id,name,description,url):
+                self.id = id
+                self.name = name
+                self.description = description
+                self.url = url
 
 class Category:
-     '''
-    Class that instantiates objects of the news categories objects of the news sources
-    '''
-def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title  
+        '''
+        Class that instantiates objects of the news categories objects of the news sources
+        '''
+        def __init__(self,author,description,time,url,image,title):
+                self.author = author
+                self.description = description
+                self.time = time
+                self.url = url
+                self.image = image
+                self.title = title  
 
--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-from app.config import Config
 import os
 
-class config:
+class Config:
     SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
