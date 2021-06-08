d7ecff1c501564f0febd106e34b2a248008fb63c
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 11:15:03 +0300

    add the changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,28 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_news_source,get_articles
+from ..models import NewsSource
 
-# Views
-@app.route('/') 
+#views
+@main.route('/')
 def index():
+	'''
+	view root page function that returns the index page and its data
+	'''
+	technology = get_news_source('technology')
+	business = get_news_source('business')
+	sports = get_news_source('sports')
+	entertainment = get_news_source('entertainment')
+	title = "News Room"
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+	return render_template('index.html',title = title, technology = technology,business = business,sports = sports,entertainment = entertainment)
+
+@main.route('/news_articles/<id>')
+def articles(id):
+	'''
+	view articles  page function that returns the articles page and its data
+	'''
+	articles = get_articles(id)
+	title = f'News Room articles | {id}'
+
+	return render_template('news_articles.html',title= title,articles = articles)
