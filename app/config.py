533912be8955e0918ed5ad72dd94af6cbc5f36ae
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 07:38:00 +0300

    modify the changes

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    MOVIE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 
 class ProdConfig(Config):
     '''
--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+from app.config import Config
 import os
 
 class config:
     SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
