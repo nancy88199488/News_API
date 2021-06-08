8df91ae7d521c157dcfd082d65c81ff841db9d18
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 08:11:11 +0300

    formart the models file

--- app/models.py
+++ app/models.py
@@ -1,12 +1,23 @@
 class Article:
     '''
-    News article class to define the news object 
+    News article class to define the newsArticle object 
     '''
     def __init__(self,author,description,time,url,image,title):
+        self.id = id
         self.author = author
         self.description = description
         self.time = time
         self.url = url
         self.image = image
         self.title = title
-        
+
+class source:
+    '''
+       News source class to define the newsSource object 
+       '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+
