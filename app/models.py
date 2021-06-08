66fb044a5ab6d29238c999960d34cfb6f7809657
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 08:04:16 +0300

    add the class news article

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class Article:
+    '''
+    News article class to define the news object 
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+        
