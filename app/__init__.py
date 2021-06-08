bb4f8845d7070b1d07c6be31744557d9b5666030
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 07:42:27 +0300

    update the  init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
 
-# Initializing application
-app = Flask(__name__)
+def create_app(config_name):
 
-from app import views
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+# setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    # Will add the views and forms
+
+    return app
