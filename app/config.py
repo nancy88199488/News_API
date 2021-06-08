8066cab24e439d35e402ff06fb963c7b2349602c
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-08 07:05:06 +0300

    update and modify the config file

---
+++ app/config.py
@@ -0,0 +1,22 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    Pruduction  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    NEWS_API_KEY = 'd1ec0f636be94766b9ae8b1995f9ca44'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
